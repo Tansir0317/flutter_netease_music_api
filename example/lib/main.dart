e8471a97abcff6cc3f2a1abed6c669fc8365f915
Tansir0317 <tansir0317@163.com>
2025-03-06 15:10:17 +0800

    feature(): init api

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,32 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_netease_music_api/flutter_netease_music_api.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NeteaseMusicApi.init(debug: true);
-  runApp(const MyApp());
+  _startServer(port: 3001);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<HttpServer> _startServer({address = "localhost", int port = 3000}) {
+  return HttpServer.bind(address, port, shared: true).then((server) {
+    print("start listen at: http://$address:$port");
+    server.listen((request) {
+      _handleRequest(request);
+    });
+    return server;
+  });
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('flutter_netease_music_api')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            ElevatedButton(onPressed: () async {
-              final res = await NeteaseMusicApi().playlistHotTags();
-              print("res---${res.toJson()}");
-            }, child: Text("发送请求"))
-            ],
-          ),
-        ),
-      ),
-    );
+void _handleRequest(HttpRequest request) async {
+  if (request.uri.path == '/favicon.ico') {
+    request.response.statusCode = 404;
+    request.response.close();
+    return;
   }
+  final answer = await cloudMusicApi(request.uri.path, parameter: request.uri.queryParameters, cookie: request.cookies).catchError((e, s) async {
+    print(e.toString());
+    print(s.toString());
+    return const Answer();
+  });
+
+  request.response.statusCode = answer.status;
+  request.response.cookies.addAll(answer.cookie);
+  request.response.write(json.encode(answer.body));
+  request.response.close();
+
+  print("request[${answer.status}] : ${request.uri}");
 }
--- lib/flutter_netease_music_api.dart
+++ lib/flutter_netease_music_api.dart
@@ -1,5 +1,38 @@
-library flutter_netease_music_api;
+import 'dart:async';
+import 'dart:io';
 
-export 'src/netease_api.dart';
 
-export 'src/netease_bean.dart';
+import 'flutter_netease_music_api.dart';
+import 'src/module.dart';
+
+export 'src/answer.dart';
+
+typedef DebugPrinter = void Function(String message);
+
+DebugPrinter debugPrint = (msg) {
+  print(msg);
+};
+
+/// 请求网易云音乐 API
+/// API文档地址参考: https://binaryify.github.io/NeteaseCloudMusicApi/#/
+Future<Answer> cloudMusicApi(
+    String path, {
+      Map? parameter,
+      List<Cookie> cookie = const [],
+    }) async {
+  assert(handles.containsKey(path), "此 api url 未被定义, 请检查: $path ");
+  final handle = handles[path];
+
+  if (handle == null) {
+    throw HttpException('404 not found');
+  }
+
+  try {
+    final answer = await handle(parameter, cookie);
+    return answer;
+  } on HttpException catch (e, stack) {
+    debugPrint(e.toString());
+    debugPrint(stack.toString());
+    rethrow;
+  }
+}
